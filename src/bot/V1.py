f8c20f0a724a54068c78f9e1c9fbeabcf854be25
Uniaut <kunwoo0927@gmail.com>
2023-03-13 02:36:54 +0900

    [Feat] Update bot command into app_command

--- src/bot/V1.py
+++ src/bot/V1.py
@@ -1,4 +1,5 @@
 import discord
+import discord.app_commands as app_commands
 import discord.ext.commands as commands
 
 import src.chatgpt.V1 as chatgpt
@@ -11,58 +12,47 @@ intents.message_content = True
 class BotWrapper():
     def __init__(self, db_url: str):
         self.db_client = database.connect_to_database(db_url)
-        self.bot = self.create_bot()
+        self.bot: discord.Client = self.create_app()
 
-    def create_bot(self) -> commands.Bot:
-        bot = commands.Bot(intents=intents, command_prefix='/')
+    def create_bot(self):
+        app = discord.Client(intents=intents, command_prefix='/')
+        tree = app_commands.CommandTree(app)
 
-        @bot.event
+        @app.event
         async def on_ready():
+            await tree.sync()
             print('ChatGPT Discord Bot is ready')
             print('Version: V1')
 
-        @bot.group(name='register')
-        async def register(ctx: commands.Context):
+        @tree.command(name='register')
+        async def register(interaction: discord.Interaction, *, token: str):
             '''
             register auth data of chatgpt from user
             '''
-            if ctx.invoked_subcommand is None:
-                await ctx.channel.send('Please specify a auth type')
-                await ctx.message.delete()
-
-        @register.command(name='access_token')
-        async def register_access_token(ctx: commands.Context, *, token: str):
-            '''
-            register access_token of chatgpt from user
-            '''
-            user_uuid = ctx.message.author.id
+            await interaction.response.send_message('...', delete_after=0)
+            user_uuid = interaction.user.id
             user_auth = {
                 'access_token': token
             }
             database.set_user_auth(self.db_client, user_uuid, user_auth)
-            await ctx.channel.send('auth data is successfully registered.')
-            await ctx.message.delete()
+            await interaction.channel.send('Auth data is successfully registered.')
 
-        @bot.command(name='chatgpt')
-        async def chatgpt_command(ctx: commands.Context, *, prompt: str):
+        @tree.command(name='chatgpt')
+        async def chatgpt_command(interaction: discord.Interaction, *, prompt: str):
             '''
             send a prompt to chatgpt, bot replies with a response
             1. get registerd user aut if not, suggest to register
             2. get chatbot instance if not, create one
             3. send prompt to chatbot instance
             4. get response from chatbot instance
-
-            params:
-                ctx: discord context
-                *args: prompt
             '''
-            user_uuid = ctx.message.author.id
+            user_uuid = interaction.user.id
             try:
                 user_auth = database.get_user_auth(self.db_client, user_uuid)
                 last_conversation = database.get_last_conversation(self.db_client, user_uuid)
             except Exception as e:
                 print(e.with_traceback(None))
-                await ctx.reply('You are not registered, please register first')
+                await interaction.response.send_message('You are not registered, please register first')
                 return
             
             try:
@@ -73,9 +63,9 @@ class BotWrapper():
                     last_conversation,
                 )
                 database.set_last_conversation(self.db_client, user_uuid, conversation_id)
-                await ctx.reply(response)
+                await interaction.response.send_message(response)
             except Exception as e:
                 print(e.with_traceback(None))
-                await ctx.reply('Something went wrong, please try again later')
-            
-        return bot
+                await interaction.response.send_message('Something went wrong, please try again later')
+
+        return app
